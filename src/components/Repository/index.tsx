57afb12e6e5ecee1cd56df57fa6fcff95f2596ac
Victor Marques <victormatheus92@gmail.com>
2021-03-11 17:38:16 -0300

    atualizado

--- src/components/Repository/index.tsx
+++ src/components/Repository/index.tsx
@@ -1,44 +1,101 @@
-import React from 'react';
-import { Card, Container, Security } from './styles';
+import React, { useState } from 'react';
+import { FcSimCardChip } from 'react-icons/fc';
+import {
+  Card,
+  Container,
+  Fill,
+  Security,
+  Base,
+  CardBack,
+  CardFront,
+  Code,
+  Number,
+} from './styles';
+import Master from '../../assets/master.png';
 
 const Repository: React.FC = () => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+  const [expire, setExpire] = useState('');
+  const [security, setSecurity] = useState('');
+  const [rotate, setRotate] = useState(false);
+
   return (
     <Container>
       <h1>Payment Information</h1>
 
-      <div>
-        <div>
-          <h1>Cartão</h1>
-        </div>
+      <Base>
+        <Card isRorate={rotate}>
+          <CardFront>
+            <span>
+              <FcSimCardChip />
 
-        <div>
-          <Card>
-            <label htmlFor="name">
-              Name <br />
-              <input type="text" />
+              <img src={Master} alt="master" />
+            </span>
+
+            <Number>
+              <h6>card number</h6>
+              <p>{number || '1234 5678 9012 3456'}</p>
+            </Number>
+
+            <Code>
+              <div>
+                <h6>cardholder Name</h6>
+                <p>{name || 'Joseph Climber'}</p>
+              </div>
+
+              <div>
+                <h6>expiration</h6>
+                <p>{expire || '12/12'}</p>
+              </div>
+            </Code>
+          </CardFront>
+
+          <CardBack>
+            <p>{security || '000'}</p>
+            <h6>expiration</h6>
+          </CardBack>
+        </Card>
+
+        <Fill>
+          <label htmlFor="name">
+            Name <br />
+            <input
+              type="text"
+              onChange={event => setName(event.target.value)}
+            />
+          </label>
+          <br />
+
+          <label htmlFor="number">
+            Card Number <br />
+            <input
+              type="text"
+              onChange={event => setNumber(event.target.value)}
+            />
+          </label>
+
+          <Security>
+            <label htmlFor="date">
+              Expiration (mm/yy) <br />
+              <input
+                type="text"
+                onChange={event => setExpire(event.target.value)}
+              />
             </label>
-            <br />
 
-            <label htmlFor="name">
-              Card Number <br />
-              <input type="text" />
+            <label htmlFor="date">
+              Security Code <br />
+              <input
+                type="text"
+                onChange={e => setSecurity(e.target.value)}
+                onFocus={() => setRotate(true)}
+                onBlur={() => setRotate(false)}
+              />
             </label>
-            <br />
-
-            <Security>
-              <label htmlFor="date">
-                Expiration (mm/yy) <br />
-                <input type="text" />
-              </label>
-
-              <label htmlFor="date">
-                Security Code <br />
-                <input type="text" />
-              </label>
-            </Security>
-          </Card>
-        </div>
-      </div>
+          </Security>
+        </Fill>
+      </Base>
     </Container>
   );
 };
--- src/components/Repository/styles.ts
+++ src/components/Repository/styles.ts
@@ -1,43 +1,159 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import Colors from '../../styles/colors';
+
+interface CardProps {
+  isRorate: boolean;
+}
+
+// ------- form ------
 
 export const Container = styled.div`
   background-color: ${Colors.greyMedium};
+  height: 100vh;
+  padding-top: 50px;
+  perspective: 1000px;
 
   h1 {
-    margin-top: 10px;
     text-align: center;
-  }
-
-  div {
-    display: flex;
-    justify-content: space-around;
-    margin-top: 30px;
+    padding-bottom: 50px;
   }
 `;
 
-export const Card = styled.form`
+export const Base = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin-top: 30px;
+`;
+
+export const Fill = styled.form`
+  align-items: center;
   display: flex;
   flex-direction: column;
-  align-items: center;
 
   label {
-    font-size: 12px;
-    line-height: 20px;
     color: ${Colors.secondary};
+    font-size: 12px;
+    letter-spacing: 0.5px;
+    line-height: 20px;
     padding-left: 10px;
   }
 
   input {
     border: 2px solid ${Colors.light};
     border-radius: 4px;
+    font-size: 16px;
     height: 40px;
+    padding: 10px;
+    transition: 0.18s ease-in-out;
     width: 490px;
+
+    &:focus {
+      border: 2px solid ${Colors.black};
+    }
   }
 `;
 
 export const Security = styled.div`
+  display: flex;
+  margin: 12px 0;
+
   input {
     width: 240px;
+  }
+`;
+
+// ---- Card ---
+
+export const Card = styled.div<CardProps>`
+  max-height: 251px;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+  display: flex;
+  align-items: center;
+  width: 400px;
+
+  h6 {
+    color: grey;
+  }
+
+  img {
+    width: 60px;
+  }
+
+  ${props =>
+    props.isRorate &&
+    css`
+      transform: rotateY(180deg);
+    `}
+`;
+
+export const CardFront = styled.div`
+  backface-visibility: hidden;
+  background-color: ${Colors.warning};
+  border-radius: 22px;
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px,
+    rgba(0, 0, 0, 0.22) 0px 10px 10px;
+  color: black;
+  height: 54vw;
+  max-height: 251px;
+  margin: 0 auto;
+  width: 400px;
+  padding: 40px;
+  position: absolute;
+
+  span {
+    display: flex;
+    font-size: 50px;
+    justify-content: space-between;
+    padding-bottom: 30px;
+  }
+`;
+
+export const Number = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 20px;
+`;
+
+export const Code = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const CardBack = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  backface-visibility: hidden;
+  border-radius: 22px;
+  background-color: ${Colors.warning};
+  box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px,
+    rgba(0, 0, 0, 0.22) 0px 10px 10px;
+  color: black;
+  height: 54vw;
+  max-height: 251px;
+  margin: 0 auto;
+  width: 400px;
+  padding: 20px;
+  position: absolute;
+  transform: rotateY(180deg);
+
+  &::before {
+    background-color: ${Colors.black};
+    content: '';
+    width: 400px;
+    height: 40px;
+    position: absolute;
+    top: 40px;
+    right: 0;
+  }
+
+  &::after {
+    content: '';
+    width: 280px;
+    height: 30px;
+    left: 20px;
+    position: absolute;
+    background-color: ${Colors.white};
   }
 `;
