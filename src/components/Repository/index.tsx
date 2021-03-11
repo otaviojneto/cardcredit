7de8f3962c7e5417fe8073cf09f71f00f2c4f577
Victor Marques <victormatheus92@gmail.com>
2021-03-11 19:57:17 -0300

    add

--- src/components/Repository/index.tsx
+++ src/components/Repository/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { FcSimCardChip } from 'react-icons/fc';
 import { visa, master, elo } from '../../assets';
 import {
@@ -14,33 +14,63 @@ import {
 } from './styles';
 
 const Repository: React.FC = () => {
-  // const [mask];
-
   const [name, setName] = useState('');
   const [number, setNumber] = useState('');
   const [expire, setExpire] = useState('');
   const [security, setSecurity] = useState('');
   const [rotate, setRotate] = useState(false);
-  const [imaster, setMaster] = useState('');
-  const [ivisa, setVisa] = useState('');
-  const [ielo, setElo] = useState('');
+  const [paymentType, setPaymentType] = useState('');
 
-  switch (master) {
-    case 'value_1':
-      imaster = <img src={master} alt="master" />;
-      break;
+  const getCardFlag = useCallback(() => {
+    console.log(number);
+    const checkCard = number.replace(/[^0-9]+/g, '');
+    let flag = '';
+    const cards = {
+      visa: /^4[0-9]{12}(?:[0-9]{3})/,
+      mastercard: /^5[1-5][0-9]{14}/,
+      diners: /^3(?:0[0-5]|[68][0-9])[0-9]{11}/,
+      amex: /^3[47][0-9]{13}/,
+      discover: /^6(?:011|5[0-9]{2})[0-9]{12}/,
+      hipercard: /^(606282\d{10}(\d{3})?)|(3841\d{15})/,
+      elo: /^((((636368)|(438935)|(504175)|(451416)|(636297))\d{0,10})|((5067)|(4576)|(4011))\d{0,12})/,
+      jcb: /^(?:2131|1800|35\d{3})\d{11}/,
+      aura: /^(5078\d{2})(\d{2})(\d{11})$/,
+    };
 
-    case 'value_2':
-      ivisa = <img src={visa} alt="visa" />;
-      break;
+    // eslint-disable-next-line no-restricted-syntax
+    for (const itemflag in cards) {
+      if (cards[itemflag].test(checkCard)) {
+        flag = itemflag;
+      }
+    }
 
-    case 'value_3':
-      ielo = <img src={elo} alt="elo" />;
-      break;
+    return setPaymentType(flag);
+  }, [number]);
 
-    default:
-      break;
-  }
+  // eslint-disable-next-line consistent-return
+  const handleFlag = () => {
+    console.log(paymentType);
+    switch (paymentType) {
+      case 'visa':
+        return <img src={visa} alt="Visa" />;
+      case 'mastercard':
+        return <img src={master} alt="Mastercard" />;
+      // case 'diners':
+      //   return <img src={Diners} alt={Diners} />
+      // case 'discover':
+      //   return <img src={Discover} alt={Discover} />
+      // case 'elo':
+      //   return <img src={Elo} alt={Elo} />
+      // case 'hipercard':
+      //   return <img src={Hipercard} alt={Hipercard} />
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    getCardFlag();
+  }, [getCardFlag]);
 
   return (
     <Container>
@@ -52,11 +82,7 @@ const Repository: React.FC = () => {
             <span>
               <FcSimCardChip />
 
-              {master === visa ? (
-                <img src={master} alt="master" />
-              ) : (
-                master
-              )}
+              {handleFlag()}
             </span>
 
             <Number>
